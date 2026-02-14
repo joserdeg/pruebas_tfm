d99e1a937bb9069847f223d0c9f01b0ac395e451
joserdg4 <joseromerodegaetano@gmail.com>
2026-02-14 12:50:27 +0100

    openneruo_db

---
+++ openneuro/openneuro_db.py
@@ -0,0 +1,8 @@
+# Exploración inicial para la DB openneuro
+import os
+
+root = "/Users/joseromerodegaetano/ds004504"
+
+# Se explora la estructura del dataset
+for item in os.listdir(root):
+    print(item)
